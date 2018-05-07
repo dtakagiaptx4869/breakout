26ad929a0810ac0ca09136e9cea2a3c1b8679ee7
Tsubasa Toki <erwincat0812@gmail.com>
2018-05-07 09:26:26 +0900

    コメントを追加

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,22 @@ char key[256];
 int main(int argc, char const* argv[]) {
     ChangeWindowMode(TRUE);
 
+    // DXライブラリ初期化
     if (DxLib_Init() == -1) return -1;
 
+    // 裏画面へ描画
     SetDrawScreen(DX_SCREEN_BACK);
 
+    // コントロールインスタンスを生成
     Control *control = new Control();
 
     while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0 && GetHitKeyStateAll(key) == 0) {
+        // ゲームオーバーでtrueを返す
         if (control->All()) break;
     }
 
+    // 終了処理
     DxLib_End();
+    delete control;
     return 0;
 }
